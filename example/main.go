a4ac30c88021603e1d77c971c8c27c5745b71f20
Dr-dddd <1620210600@qq.com>
2023-07-17 10:14:21 +0800

    :white_check_mark: add RegisterMultipleSubscriber
    add RegisterMultipleSubscriber method and return err -> panic

--- example/main.go
+++ example/main.go
@@ -12,17 +12,12 @@ func main() {
 	zlog.New(zlog.LevelDev)
 	o := mqc.NewOptions("tcp://100.100.10.13:1883", "go_test_client", "goTest", "123456")
 	mqc.New(o, zlog.Log())
-	err := mqc.RegisterSubscriber("test1", mqc.ExactlyOnce, func(client mqtt.Client, message mqtt.Message) {
+	mqc.RegisterSubscriber("test1", mqc.ExactlyOnce, func(client mqtt.Client, message mqtt.Message) {
 		fmt.Printf("TOPIC: %s\n", message.Topic())
 		fmt.Printf("MSG: %s\n", message.Payload())
 	})
-	if err != nil {
-		panic(err)
-	}
-	mqc.ServerConnect()
-	err = mqc.Publish("test1", mqc.ExactlyOnce, false, []byte("this is a test message!"))
-	if err != nil {
-		panic(err)
+	for i := 0; i < 100; i++ {
+		mqc.Publish("test1", mqc.ExactlyOnce, false, []byte("this is a test message!"))
 	}
 	defer mqc.ServerDisconnect(0)
 	time.Sleep(time.Second * 5)
--- mqc_test.go
+++ mqc_test.go
@@ -13,20 +13,37 @@ func BenchmarkParallel(b *testing.B) {
 
 	o := NewOptions("tcp://100.100.10.13:1883", "go_test_client", "goTest", "123456")
 	New(o, zlog.Log())
-	err := RegisterSubscriber("test1", ExactlyOnce, func(client mqtt.Client, message mqtt.Message) {
+	RegisterSubscriber("test1", ExactlyOnce, func(client mqtt.Client, message mqtt.Message) {
 		fmt.Printf("MSG: %s\n", message.Payload())
 	})
-	if err != nil {
-		panic(err)
-	}
 	b.SetParallelism(10)
 	b.RunParallel(func(pb *testing.PB) {
 		for i := 0; i < b.N; i++ {
 			fmt.Print(i)
-			err = Publish("test1", ExactlyOnce, false, []byte(strconv.FormatInt(int64(i), 10)))
-			if err != nil {
-				panic(err)
-			}
+			Publish("test1", ExactlyOnce, false, []byte(strconv.FormatInt(int64(i), 10)))
 		}
 	})
+}
+
+func TestRegisterMultipleSubscriber(t *testing.T) {
+	zlog.New()
+	o := NewOptions("tcp://100.100.10.13:1883", "go_test_client", "goTest", "123456")
+	New(o, zlog.Log())
+	topics := map[string]byte{
+		"test1": byte(ExactlyOnce),
+		"test2": byte(ExactlyOnce),
+	}
+
+	var receiveMessage []string
+
+	RegisterMultipleSubscriber(topics, func(client mqtt.Client, message mqtt.Message) {
+		fmt.Printf("MSG: %s\n", message.Payload())
+		receiveMessage = append(receiveMessage, string(message.Payload()))
+	})
+
+	Publish("test1", ExactlyOnce, false, []byte("test1:123"))
+	Publish("test2", ExactlyOnce, false, []byte("test2:456"))
+	fmt.Println(receiveMessage)
+	//assert.Equal(t, "test1:123", receiveMessage[0])
+	//assert.Equal(t, "test2:456", receiveMessage[1])
 }
